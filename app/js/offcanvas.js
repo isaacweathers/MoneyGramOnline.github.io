6cfdb611eca5d5aec2d201f2f3af0c49edfee5ec
Rich Sollenne <Rich@Sollenne.com>
2016-07-15 14:21:45 -0500

    fix menu | add favicon and apple touch images

--- app/js/offcanvas.js
+++ app/js/offcanvas.js
@@ -1,29 +1,31 @@
-$(document).ready(function($) {
-  $("#menu").mmenu({
-    "extensions": [
-      "pagedim-black",
-      "theme-dark"
-    ],
-    "offCanvas": {
-      "position": "right",
-      "zposition": "front",
-      pageSelector: ".content"
-    },
-    "searchfield": {
-      "resultsPanel": true,
-      "showTextItems": true
-    },
-    "navbars": [
-      {
-        "position": "top",
-        "content": [
-          "searchfield"
-        ]
+$(document).ready(function() {
+  $(function() {
+    $('nav#menu').mmenu({
+      "extensions": [
+        "pagedim-black",
+        "theme-dark"
+      ],
+      "offCanvas": {
+        "position": "right",
+        "zposition": "front",
+        "pageSelector": ".content"
+      },
+      "searchfield": {
+        "resultsPanel": true,
+        "showTextItems": true
+      },
+      "navbars": [
+        {
+          "position": "top",
+          "content": [
+            "searchfield"
+          ]
+        }
+      ]
+    }, {
+      "searchfield": {
+        "clear": true
       }
-    ]
-  }, {
-    "searchfield": {
-      "clear": true
-    }
+    });
   });
 });
